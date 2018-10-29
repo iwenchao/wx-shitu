39b9a957f07bb1d04c77f0bc25f3c62e1d682b25
iwen <iwenchaos@gmail.com>
2018-10-29 20:40:03 +0800

    增加菜品识别页面

--- pages/dish/dish.js
+++ pages/dish/dish.js
@@ -1,66 +1,153 @@
 // pages/dish/dish.js
+const api = require('../../utils/api.js');
+const dishUrl = api.getDishUrl();
+
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        //用户数据
+        userInfo:{},
+        backUserInfo:{},
+        hasUserInfo:false,
+        openId:"",
+        nickName:"",
 
-  },
+        //识别数据
+        imageUrl: "",
+        errInfo:"",
+        dishName:"",
+        calore:"",
+        probability:"",
+        description:""
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        let gUserInfo = app.globalData.userInfo;
+        this.setData({
+            userInfo:gUserInfo,
+            hasUserInfo:true,
+            openId:gUserInfo.openId,
+            nickName:gUserInfo.nickName
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    upload: function () {
+        let that = this;
+        wx.chooseImage({
+            count: 1,
+            sizeType: ["compressed"],
+            sourceType: ["album", "camara"],
+            success: function (res) {
+                that.setData({
+                    imageUrl: res.tempFilePaths[0],
+                }),
+                    wx.showLoading({
+                        mask: true,
+                        title: "玩命识别中..."
+                    }),
+                    wx.uploadFile({
+                        url: dishUrl,
+                        filePath: res.tempFilePaths[0],
+                        name: "file",
+                        header: {
+                            'content-type': 'multipart/form-data',
+                        },
+                        formData: {
+                            'openId': that.data.openId,
+                            'nickName': that.data.nickName
+                        },
+                        success: function (res) {
+                            wx.hideLoading();
+                            let jsonData = res.data;
+                            let result = JSON.parse(jsonData);
+                            if (result.code == "0") {
+                                that.setData({
+                                    dishName:result.dishName,
+                                    calore:result.calore,
+                                    probability:result.probability,
+                                    description:result.description
+                                })
+                            } else if (result.code == "1") {
+                                that.setData({
+                                    info: "Sorry "+result.msg
+                                })
+                            }else{
+                                that.setData({
+                                    info: "Sorry, 不能为所欲为了"
+                                })
+                            }
 
-  },
+                        }
+                    })
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
 
-  },
+            },
+            fail: function (res) {
+                wx.hideLoading();
+                wx.showModel({
+                    title:"温馨提示",
+                    content:"Sorry,不能为所欲为了",
+                    showCancel:false
+                })
+            },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    },
 
-  }
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
--- utils/api.js
+++ utils/api.js
@@ -7,23 +7,32 @@ var clientType = "wsc";
 
 /**接口api*/
 //授权获取微信用户信息
-const oauthUrl = host+"/wcsp/oauth";
-function getOauthUrl(){
-  return oauthUrl;
+const oauthUrl = host + "/wcsp/oauth";
+function getOauthUrl() {
+    return oauthUrl;
 }
 
 //花卉api
 const flowerUrl = host + "/rest/icr/detect?clientType=" + clientType + '&apiType=flower&authCode=' + authCode;
 function getFlowerUrl() {
-  return flowerUrl;
+    return flowerUrl;
 }
 //植物识别url
 const plantUrl = host + '/rest/icr/detect?clientType=' + clientType + '&apiType=plant&authCode=' + authCode;
-function getPlantUrl(){
+function getPlantUrl() {
     return plantUrl;
 }
+//菜品识别
+const dishUrl = host + '/rest/icr/detect?clientType=' + clientType + '&apiType=dish&authCode=' + authCode;
+function getDishUrl() {
+    return dishUrl;
+}
+
+
+
 
 /**对外暴露的常量函数 */
 module.exports.getOauthUrl = getOauthUrl;
 module.exports.getFlowerUrl = getFlowerUrl;
 module.exports.getPlantUrl = getPlantUrl;
+module.exports.getDishUrl = getDishUrl;
