38395d3bf088acb685ffb9a44df5491729ef35c1
iwen <iwenchaos@gmail.com>
2018-10-30 14:27:16 +0800

    遗留文件上传;

---
+++ utils/constant.js
@@ -0,0 +1,6 @@
+export const page_dish = "0";
+export const page_car = "1";
+export const page_plant = "2";
+export const page_animal = "3";
+export const page_logo = "4";
+export const page_facesticker = "5";
